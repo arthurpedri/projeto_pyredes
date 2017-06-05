aafb7eb97ff5a6eb4f771cfd3778c411663752e5
Arthur Pedri Trevisol <arthur.pedri@gmail.com>
2017-06-05 02:57:24 -0300

    Alguns Comentarios
    Signed-off-by: Arthur Pedri Trevisol <arthur.pedri@gmail.com>

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import sys
 from threading import Thread
 import time
 
-class Hosts:
+class Hosts: # Classe para usar na lista de hosts, contendo nome e id
     def __init__(self, name, id):
         self.name = name
         self.id = id
 
-def novoLider(hosts, id):
+def novoLider(hosts, id): # Funcao para determinar um lider novo com base na lista de hosts e no proprio id
     menor = int(id)
     for host in hosts:
         if int(host.id) < int(menor):
@@ -40,11 +40,11 @@ if len(sys.argv) < 2:
     print "Parametros errados:<N> <ID(local)> <host> <id> <host> <id> ...."
     exit()
 
-n = sys.argv[1]
-ID = sys.argv[2]
-hosts = list()
+n = sys.argv[1] # numero de hosts
+ID = sys.argv[2] # id do host local
+hosts = list() # lista de hosts fora o local
 for i in range(3, len(sys.argv),2):
-    h = Hosts(sys.argv[i], sys.argv[i + 1])
+    h = Hosts(sys.argv[i], sys.argv[i + 1]) # adicionar um host a lista de hosts com seu nome e id
     hosts.append(h)
     print i
 lider = 0
@@ -66,18 +66,18 @@ while 1:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         time.sleep(1)
 
-        if s.connect_ex((host.name, TCP_PORT)):
+        if s.connect_ex((host.name, TCP_PORT)): # verificar se alguma conexao foi fechada
             print host.name, " Desconectado"
             m = host.name + " Perdeu Conexao MSG DE: " + ID
-            if int (host.id) == int (lider):
-                hosts.remove(host)
-                lider = novoLider(hosts, ID)
-                mLider = "Novo lider e: " + str(lider) + " MSG DE: " + ID
-                for remaining in hosts:
+            if int (host.id) == int (lider): # ajustes para quando o host desconectado foi o lider
+                hosts.remove(host) # remove o desconectado da lista de hosts
+                lider = novoLider(hosts, ID) # chama a funcao para definir o novo lider
+                mLider = "Novo lider e: " + str(lider) + " MSG DE: " + ID # prepara a mensagem informando quem e o novo lider
+                for remaining in hosts: # manda a mensagem informando quem e o novo lider
                     s.connect_ex((remaining.name, TCP_PORT))
                     s.send(mLider, socket.MSG_OOB)
                     s.close()
-            else:
+            else: # caso o host desconectado nao seja o lider so manda mensagem informando o host desconectado
                 hosts.remove(host)
                 for remaining in hosts:
                     s.connect_ex((remaining.name, TCP_PORT))
