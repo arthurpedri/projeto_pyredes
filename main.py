dd9526cfc130aa028bcad594d840e9fbe78560c4
Arthur Pedri Trevisol <arthur.pedri@gmail.com>
2017-06-05 22:42:52 -0300

    comentarios
    Signed-off-by: Arthur Pedri Trevisol <arthur.pedri@gmail.com>

--- main.py
+++ main.py
@@ -18,25 +18,25 @@ def novoLider(hosts, id): # Funcao para determinar um lider novo com base na lis
             menor = host.id
     return menor
 
-def listener(n, id):
+def listener(n, id): # funcao aberta com thread para escutar a porta 5005
     TCP_IP = ''
     TCP_PORT = 5005
     BUFFER_SIZE = 1024
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # abrindo socket
     s.bind((TCP_IP, TCP_PORT))
-    s.listen(int(n))
+    s.listen(int(n)) # especificando a quantidade de peers para se conectarem
 
     while 1:
-        conn, addr = s.accept()
+        conn, addr = s.accept() # aceita uma conexao tcp
         # print id, ': Connection address:', addr
-        data = conn.recv(BUFFER_SIZE)
+        data = conn.recv(BUFFER_SIZE) # coloca o conteudo da mensagem na variavel data
         # if not data: break
-        print datetime.datetime.utcnow(), "DE ", id, ": >> :", data
-        conn.close()
+        print datetime.datetime.utcnow(), "DE ", id, ": >> :", data # imprime os dados com formatacao para arquivo de log
+        conn.close() # fecha a conexao tcp
 
 
-if len(sys.argv) < 2:
+if len(sys.argv) < 2: # verificacao para facil demonstracao de uso do programa
     print "Parametros errados:<N> <ID(local)> <host1> <id1> <host2> <id2> ... <hostN> <idN>"
     exit()
 
@@ -46,22 +46,22 @@ hosts = list() # lista de hosts fora o local
 for i in range(3, len(sys.argv),2):
     h = Hosts(sys.argv[i], sys.argv[i + 1]) # adicionar um host a lista de hosts com seu nome e id
     hosts.append(h)
-    print i
+    #print i
 
-t = Thread(target=listener, args=(n,ID,))
-t.start()
+t = Thread(target=listener, args=(n,ID,)) # declara a thread que vai usar a funcao listener
+t.start() # abre a thread
 
 # TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
-HEARTBEAT = "Heartbeat from "+ID
+HEARTBEAT = "Heartbeat from "+ID # determina a mensagem de heartbeat
 
-time.sleep(5)
+#time.sleep(5)
 ##ELEICAO
 
 ELEICAO = "ESTADO DE ELEICAO MSG DE " + ID
 ALL_UP = False
-while not ALL_UP:
+while not ALL_UP: # enquanto todos os hosts nao estiverem conectados o programa nao vai sair desse while
     ALL_UP = True
     for host in hosts: # Percorre todos os peers para saber quem realmente esta ativo
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -71,7 +71,7 @@ while not ALL_UP:
         else:
             ALL_UP = False
 
-lider = novoLider(hosts, ID) # Com a lista atualizada, defini o novo lider
+lider = novoLider(hosts, ID) # Com a lista atualizada, define o novo lider
 
 
 while 1:
@@ -80,7 +80,7 @@ while 1:
         time.sleep(1)
 
         if s.connect_ex((host.name, TCP_PORT)): # verificar se alguma conexao foi fechada
-            print host.name, " Desconectado"
+            print host.name, " Desconectado" # avisa o usuario que o host foi desconectado
             m = host.name + " Perdeu Conexao MSG DE: " + ID
             if int (host.id) == int (lider): # ajustes para quando o host desconectado foi o lider
                 hosts.remove(host) # remove o desconectado da lista de hosts
@@ -91,21 +91,21 @@ while 1:
                         s.send(ELEICAO)
                         s.close()
                     else:
-                        hosts.remove(elect)
+                        hosts.remove(elect) # se alguma outra conexao foi fechada o host sera removido da lista de hosts e ele nao sera candidato a lideranca
 
                 lider = novoLider(hosts, ID) # chama a funcao para definir o novo lider
                 mLider = "Novo lider e: " + str(lider) + " MSG DE: " + ID # prepara a mensagem informando quem e o novo lider
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 for remaining in hosts: # manda a mensagem informando quem e o novo lider
                     s.connect_ex((remaining.name, TCP_PORT))
-                    s.send(mLider, socket.MSG_OOB)
+                    s.send(mLider, socket.MSG_OOB) # manda a mensagem informando o novo lider com tag de mensagem urgente
                     s.close()
             else: # caso o host desconectado nao seja o lider so manda mensagem informando o host desconectado
-                hosts.remove(host)
-                for remaining in hosts:
+                hosts.remove(host) # remove o host da lista
+                for remaining in hosts: # informa aos restantes que o host foi desconectado
                     s.connect_ex((remaining.name, TCP_PORT))
                     s.send(m)
                     s.close()
             break
-        s.send(HEARTBEAT)
-        s.close()
+        s.send(HEARTBEAT) # manda o heartbeat para a rede
+        s.close() # fecha o socket
